47c070897e8697284cf720e62c7f98a2c6562924
dnnzz <l1611012074@stud.sdu.edu.tr>
2021-02-14 20:54:59 +0300

    --

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import {useEffect, useState } from "react";
+import axios from "axios";
+import CoinList from "./CoinList";
 
 function App() {
+  const [data, setData] = useState([]);
+  const[loading,setLoading] = useState(true);
+  async function fetchData(){
+    try{
+      setLoading(true);
+      const result = await axios(
+        'https://api.coinlore.net/api/tickers/?start=0&limit=10',
+      );
+      setData(result.data);
+    } finally{
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+   
+    fetchData();
+    console.log("done");
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="row">
+        {loading == true ?<h1>Veriler yükleniyor...</h1>:data.data.map((item) => (
+         <CoinList {...item}  />
+    ))}
     </div>
   );
 }
